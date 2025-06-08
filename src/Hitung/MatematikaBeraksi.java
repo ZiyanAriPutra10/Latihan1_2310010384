d2b52f02100678599b62dee325cce11eb4f15586
ZiyanAriPutra10 <ziyanariputra@gmail.com>
2025-06-08 19:51:30 +0800

    Membuat class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // Membuat objek Matematika dengan nama panggilan dan parameter 8, 4
+        Matematika Ari = new Matematika(8, 4);
+
+        // Menampilkan hasil dari setiap operasi
+        System.out.println("Hasil Penjumlahan: " + Ari.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + Ari.setPengurangan());
+        System.out.println("Hasil Perkalian: " + Ari.setPerkalian());
+        System.out.println("Hasil Pembagian: " + Ari.setPembagian());
+    }
+ 
 }
