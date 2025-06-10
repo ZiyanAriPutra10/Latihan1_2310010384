46fa25abb55249bc7d949c89bb047ee1299b3793
ZiyanAriPutra10 <ziyanariputra@gmail.com>
2025-06-10 13:37:35 +0800

    membuat objek

---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,14 @@
+package latihan2;
+
+public class DataDiriBeraksi {
+
+    public static void main(String[] args) {
+        // Membuat objek dari class DataDiri dan langsung mengisi data lewat konstruktor
+        DataDiri biodata = new DataDiri("Ari", "2310010384", "4B");
+
+        // Menampilkan data menggunakan method tampilkanData()
+        biodata.tampilkanData();
+    }
+}
+    
+
